d8cef4571eee9f5a8c96d5dc55c0c133ff885935
Andrew Williams <1williamsaj@gmail.com>
2019-10-07 22:34:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,19 +2,19 @@ class Artist
 
   attr_accessor :name # Create our attribute for the artist's name.
 
-  @@all = [] # Create a global variable to store all our our initialized artists. 
+  @@all = [] # Create a global variable to store all our our initialized artists.
 
-  def initialize(name)
+  def initialize(name) # First, we initialize our Artist class with the name attribute.
     @name = name
-    @@all << self
+    @@all << self # We take our class instance and push it into our class variable.
   end
 
-  def self.all
+  def self.all # A class method that will return the contents of our class variable '@@all'
     @@all
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def songs # Second, we create our 'songs' method for returning all songs associated with a specific artist.
+    Song.all.select {|song| song.artist == self} # 
   end
 
   def add_song(song)
